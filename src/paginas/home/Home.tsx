97c4b6a50f73e314c61a412e7e7c1726df540a87
FernandoFCardoso <advfernandofcardoso@gmail.com>
2021-11-26 16:25:39 -0300

    Exercicio 1

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://pm1.narvii.com/6828/7528e49813683252ce939bea139ce2eaf0b2ec87v2_hq.jpg" alt="Imagem Tela Inicial"className="img"/> 
+        </>
+    );
+}
+
+export default Home;
