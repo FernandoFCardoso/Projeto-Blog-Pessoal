98b720071c9e2c9151519a7c54764c063c780899
FernandoFCardoso <advfernandofcardoso@gmail.com>
2021-12-01 17:59:38 -0300

    Implementação do o sistema de rotas

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -2,40 +2,43 @@ import React from "react";
 import InstagramIcon from '@material-ui/icons/Instagram'; 
 import GitHubIcon from '@mui/icons-material/GitHub'; import LinkedInIcon from '@mui/icons-material/LinkedIn'; 
 import { Grid, Box, Typography } from "@material-ui/core";
+import './Footer.css';
 
-function Footer (){
 
+function Footer() {
     return (
-    <>
-    <Grid container direction="row" justifyContent="center" alignItems="center">
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Me siga nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/eidisato" target="_blank">
-                                <GitHubIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://github.com/FernandoFCardoso" target="_blank">
+                                <GitHubIcon className='redes' />
+                                </a>
+                            <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                                <InstagramIcon className='redes' />
                             </a>
-                            <a href="https://www.instagram.com/eidisato/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/in/vinicius-eidi-sato/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                                <LinkedInIcon className='redes' />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#000000", height: "60px" }}>
+                    <Box className='box2'>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2021 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2020 Copyright:</Typography>
                         </Box>
                         <Box>
-                            <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">contato.viniciuseidisato@gmail.com</Typography>
+                            <a target="_blank" href="https://brasil.generation.org">
+                                <Typography variant="subtitle2" gutterBottom className='textos' align="center">Fernando Cardoso</Typography>
+                            </a>
                         </Box>
                     </Box>
                 </Grid>
             </Grid>
-    </>
+        </>
     )
 }
 
