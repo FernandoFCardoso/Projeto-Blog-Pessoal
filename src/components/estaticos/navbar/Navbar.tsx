57f767acc09c9ea59e06db05d1d9b02f2ffce196
FernandoFCardoso <advfernandofcardoso@gmail.com>
2021-12-07 14:13:07 -0300

    Autenticação do Blog Pessoal com React

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,54 +1,64 @@
-import React from "react";
-import { AppBar,Box,Typography,Toolbar} from "@material-ui/core";
-import {Link} from "react-router-dom";
-import "./Navbar.css";
-import { color } from "@mui/system";
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom'
+import useLocalStorage from 'react-use-localstorage';
 
+import './Navbar.css'
 function Navbar() {
+    const [token, setToken] = useLocalStorage('token');
+    let history = useHistory();
 
+    function goLogout() {
+        setToken('')
+        alert("Usuário deslogado")
+        history.push('/login')
+    }
     return (
         <>
-            <AppBar position="static" className="cor-nave-bar">
+            <AppBar position="static">
                 <Toolbar variant="dense">
-                    <Box  className="cursor">
+                    <Box className='cursor'>
                         <Typography variant="h5" color="inherit">
-                            Blog Pessoal
+                            BlogPessoal
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                Home
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                Publicar Postagem
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                Cadastrar Tema
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                Postagens
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                Temas
-                            </Typography>
-                        </Box>
-                        <Link to="/login" className="text-decorator-none">
-                            <Box mx={1} className="cursor">
+                        <Link to="/home" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
-                                    Logout
+                                    home
                                 </Typography>
                             </Box>
                         </Link>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    postagens
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/temas" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    temas
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/formularioTema" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    cadastrar tema
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Box mx={1} className='cursor' onClick={goLogout}>
+                            <Typography variant="h6" color="inherit">
+                                logout
+                            </Typography>
+                        </Box>
 
                     </Box>
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import {Grid, Typography,Box,Button} from "@material-ui/core"; 
-import "./Home.css";
-
-
+import React from 'react';
+import {Typography, Box, Grid, Button} from '@material-ui/core';
+import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
+import './Home.css';
 
 function Home() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa || cor-home'>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
@@ -23,6 +22,7 @@ function Home() {
                     <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
                 <Grid xs={12} className='postagens'>
+                    <TabPostagem />
                 </Grid>
             </Grid>
         </>
